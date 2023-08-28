6022593aab971628df9324de7b3310a7f5f834f8
Dmitriy Belov <belovdk@gmail.com>
2023-08-28 20:55:25 +0700

    Initial commit

---
+++ Tests/models.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Product:
+    name: str
+    price: float
+    description: str
+    quantity: int
+
+    def check_quantity(self, quantity) -> bool:
+        return self.quantity >= quantity
+
+    def buy(self, quantity):
+        if self.check_quantity(quantity):
+            self.quantity -= quantity
+        else:
+            raise ValueError(f'Не хватает {self.name}')
+
+
+@dataclass
+class Cart:
+    products: dict[Product, int]
+
+    def __int__(self):
+        self.products = {}
+
+    def add_product(self, product: Product, buy_count=1):
+        if product in self.products:
+            self.products[product] += buy_count
+        else:
+            self.products[product] = buy_count
+
+    def remove_product(self, product: Product, remove_count=None):
+        if product in self.products:
+            if remove_count is None or remove_count >= self.products[product]:
+                del self.products[product]
+            else:
+                self.products[product] -= remove_count
+
+    def clear(self):
+        self.products.clear()
+
+    def get_total_price(self) -> float:
+        total_price = 0
+        for product, quantity in self.products.items():
+            total_price += product.price * product.quantity
+        return total_price
+
+    def buy(self):
+        for product, quantity in self.products.items():
+            if not product.check_quantity(quantity):
+                raise ValueError(f'Не хватает {product.name}')
+            product.buy(quantity)
+
+        self.clear()
